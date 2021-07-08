137f19652430cf47767486f246d671c6c2b9fcec
varunAditya-28 <82022191+varunAditya-28@users.noreply.github.com>
2021-07-08 16:06:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var gameState;
+var backgroundImg,flightImg;
+var flight,bg;
+var game;
+var cloud,cloudsGroup,cloudImg;
+var flightBlastImg;
+
+function preload(){
+backgroundImg = loadImage("images/background.jpg")
+flightImg = loadImage("images/flight.png")
+cloudImg = loadImage("images/cloud.png")
+flightBlastImg = loadImage("images/blast.png")
+}
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  gameState = "play"
+  game = new Game();
+  cloudsGroup = new Group();
+  bg = createSprite(displayWidth/2,displayHeight/2)
+  bg.addImage(backgroundImg)
+  bg.scale = 1.5
+  bg.velocityX = -8;
+  if(bg.x >750){
+    bg.x = 100
+  }
+
+  flight = createSprite(displayWidth/5,displayHeight/4)
+  flight.addImage(flightImg)
+  flight.scale = 0.15
+}
+
+function draw() {
+ background(0); 
+  
+  
+  if(gameState === "play"){
+    game.play();
+  }
+
+  if(gameState === "end"){
+    game.end();
+  }
+}
